0e9db5a0523579b5bd40aef465cdc48f55abdd8c
mnater <mathiasnater@gmail.com>
2018-05-13 22:44:13 +0200

    update version number

--- Hyphenopoly_Loader.js
+++ Hyphenopoly_Loader.js
@@ -1,4 +1,4 @@
-/** @license Hyphenopoly_Loader 1.0.0 - client side hyphenation for webbrowsers
+/** @license Hyphenopoly_Loader 1.0.1 - client side hyphenation for webbrowsers
  *  ©2018  Mathias Nater, Zürich (mathiasnater at gmail dot com)
  *  https://github.com/mnater/Hyphenopoly
  *
