84c022fd15a2a393d4a373d648b2bff13f389179
mnater <mathiasnater@gmail.com>
2018-05-18 23:16:57 +0200

    extend test 13

--- Hyphenopoly_Loader.js
+++ Hyphenopoly_Loader.js
@@ -186,7 +186,7 @@
 
 
     // Wasm feature test with iOS bug detection (https://bugs.webkit.org/show_bug.cgi?id=181781)
-    const isWASMsupported = !(function featureTestWASM() {
+    const isWASMsupported = (function featureTestWASM() {
         if (typeof WebAssembly === "object" && typeof WebAssembly.instantiate === "function") {
             const module = new WebAssembly.Module(Uint8Array.from([0, 97, 115, 109, 1, 0, 0, 0, 1, 6, 1, 96, 1, 127, 1, 127, 3, 2, 1, 0, 5, 3, 1, 0, 1, 7, 8, 1, 4, 116, 101, 115, 116, 0, 0, 10, 16, 1, 14, 0, 32, 0, 65, 1, 54, 2, 0, 32, 0, 40, 2, 0, 11]));
             if (WebAssembly.Module.prototype.isPrototypeOf(module)) {
