d0ba5c000ffb8812a9a90da9002e3dcaea39f54c
mnater <mathiasnater@gmail.com>
2018-07-06 00:22:27 +0200

    test cacheFeatureTests, remove potentially unnessesary if

--- Hyphenopoly_Loader.js
+++ Hyphenopoly_Loader.js
@@ -509,11 +509,6 @@
                 H.clientFeat.polyfill = true;
                 H.clientFeat.langs[lang] = "H9Y";
                 loadRessources(lang);
-            } else if (
-                H.clientFeat.langs[lang] &&
-                H.clientFeat.langs[lang] === "H9Y"
-            ) {
-                loadRessources(lang);
             } else {
                 tester.createTest(lang);
             }
--- testsuite/testdriver.js
+++ testsuite/testdriver.js
@@ -27,7 +27,8 @@
         {exec: true, path: "test19.html"},
         {exec: true, path: "test20.html"},
         {exec: true, path: "test21.html"},
-        {exec: true, path: "test22.html"}
+        {exec: true, path: "test22.html"},
+        {exec: true, path: "test23.html"}
     ];
     var testframe = document.getElementById("testframe");
     var currentTest = 1;
