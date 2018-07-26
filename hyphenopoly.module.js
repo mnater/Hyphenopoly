494b67e55a4fefb5dc94ca89c09e8f8f88005f85
mnater <mathiasnater@gmail.com>
2018-07-26 12:58:47 +0200

    fix typo

--- hyphenopoly.module.js
+++ hyphenopoly.module.js
@@ -2,7 +2,6 @@
  * @license Hyphenopoly.module.js 2.3.0 - hyphenation for node
  *  ©2018  Mathias Nater, Zürich (mathiasnater at gmail dot com)
  *  https://github.com/mnater/Hyphenopoly
->>>>>>> master
  *
  * Released under the MIT license
  * http://mnater.github.io/Hyphenopoly/LICENSE
