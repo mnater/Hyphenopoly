247862715e7a260d577d6f1f15b6e6a375af2e18
mnater <mathiasnater@gmail.com>
2019-06-27 16:44:18 +0200

    remove manual replacement of mut globals (wasm)

--- tools/compileWASM.sh
+++ tools/compileWASM.sh
@@ -8,12 +8,7 @@ WASTNAME="$1.wast"
 WASMNAME="$1.wasm"
 
 echo 'running asm2wasm...'
-#./third-party/binaryen/bin/asm2wasm $SRCFILE -O4 -m 2097152 -mm 16777216 > $WASTNAME
-
-# until https://github.com/WebAssembly/binaryen/issues/2011 is solved
-# lets replace mutable globals manually
-./third-party/binaryen/bin/asm2wasm $SRCFILE -O4 -m 2097152 -mm 16777216\
-| sed 's/$asm2wasm$import// ; /^ (global.*$/d' > $WASTNAME
+./third-party/binaryen/bin/asm2wasm $SRCFILE -O4 -m 2097152 -mm 16777216 > $WASTNAME
 
 echo 'optimize > WASM...'
 ./third-party/binaryen/bin/wasm-opt $WASTNAME -O4 -o $WASMNAME
