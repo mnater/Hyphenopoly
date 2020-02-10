5fd127e7a60fa064f1e2f81e4e660d6a63b2c6bd
mnater <mathiasnater@gmail.com>
2020-02-11 00:24:50 +0100

    chore: lint .ts

---
+++ src/g.ts
@@ -0,0 +1,11 @@
+export const to: i32 = 5060952;
+export const po: i32 = 5061114;
+export const pl: i32 = 55096;
+export const vs: i32 = 1280;
+export const pt: i32 = 48904;
+export const wo: i32 = 5059884;
+export const tw: i32 = 5060012;
+export const hp: i32 = 5060076;
+export const hw: i32 = 5060140;
+export const lm: i32 = 1;
+export const rm: i32 = 2;
