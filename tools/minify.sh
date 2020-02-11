a0205ad309bab7a3e2f7ec52f5101ecd304a8e2b
mnater <mathiasnater@gmail.com>
2020-02-11 22:44:10 +0100

    chore: compare sizes when minifying

--- tools/minify.sh
+++ tools/minify.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 # -*- coding: utf-8 -*-
 
+LOADEROSIZE=$(wc -c min/Hyphenopoly_Loader.js)
+H9YOSIZE=$(wc -c min/Hyphenopoly.js)
+
+echo 'old sizes:'
+echo $LOADEROSIZE
+echo $H9YOSIZE
+
 mkdir -p min
 
 terser Hyphenopoly_Loader.js -o min/Hyphenopoly_Loader.js --comments -c -m --warn
 terser Hyphenopoly.js -o min/Hyphenopoly.js --comments -c -m --warn
+
+echo 'new sizes:'
 wc -c min/Hyphenopoly_Loader.js
 wc -c min/Hyphenopoly.js
 
