2eb3555b9fa6edf5ffce68777d97ce81785c5a38
mnater <mathiasnater@gmail.com>
2020-05-18 16:46:01 +0200

    reflect latest changes in doc examples
    [skip travis-ci]

--- docs/min/testsuite/testdriver.js
+++ docs/min/testsuite/testdriver.js
@@ -25,7 +25,7 @@
         {"exec": true, "path": "test18.html"},
         {"exec": true, "path": "test19.html"},
         {"exec": true, "path": "test20.html"},
-        {"exec": false, "path": "test21.html"},
+        {"exec": true, "path": "test21.html"},
         {"exec": true, "path": "test22.html"},
         {"exec": true, "path": "test23.html"},
         {"exec": true, "path": "test24.html"},
@@ -41,7 +41,7 @@
         {"exec": true, "path": "test34.html"},
         {"exec": true, "path": "test35.html"},
         {"exec": true, "path": "test36.html"},
-        {"exec": true, "path": "test37.html"},
+        {"exec": false, "path": "test37.html"},
         {"exec": true, "path": "test38.html"},
         {"exec": true, "path": "test39.html"},
         {"exec": true, "path": "test40.html"},
@@ -49,7 +49,8 @@
         {"exec": true, "path": "test42.html"},
         {"exec": true, "path": "test43.html"},
         {"exec": true, "path": "test44.html"},
-        {"exec": true, "path": "test45.html"}
+        {"exec": true, "path": "test45.html"},
+        {"exec": true, "path": "test46.html"}
     ];
     var testframe = document.getElementById("testframe");
     var currentTest = 1;
