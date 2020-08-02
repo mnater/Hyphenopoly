8b40f8d4b5129ceb87c74f0a80f26c6836508682
mnater <mathiasnater@gmail.com>
2020-08-02 14:38:19 +0200

    fix: keep "-" in alphabet

--- Hyphenopoly.js
+++ Hyphenopoly.js
@@ -762,7 +762,7 @@
             if (!H.languages) {
                 H.languages = new Map();
             }
-            alphabet = alphabet.replace(/-/g, "");
+            alphabet = alphabet.replace(/-/g, "\\-");
             H.languages.set(lang, {
                 alphabet,
                 "cache": new Map(),
