90facef52af0c261566dfbc61885f6ae493c656e
mnater <mathiasnater@gmail.com>
2021-10-10 21:46:04 +0200

    chore: recompile all wasm

--- lang/nn/src/mytransform.js
+++ lang/nn/src/mytransform.js
@@ -2,53 +2,6 @@
 /* eslint-disable security/detect-non-literal-fs-filename */
 /* eslint-env node */
 
-/*
- * Memory layout (static)
- *
- * #--------------------# <- Offset 0
- * |        word        |
- * | 64 * Uint16 = 128B |
- * #--------------------# <- 128
- * |   translatedWord   |
- * |  64 * Uint8 = 64B  |
- * #--------------------# <- 192
- * |    hyphenPoints    |
- * |  64 * Uint8 = 64B  |
- * #--------------------# <- 256
- * |    translateMap    |
- * |         keys:      |
- * | 256 chars * 2Bytes |
- * |          +         |
- * |       values:      | 1024B
- * | 256 chars * 1Byte  |
- * |          +         |
- * |     collisions:    |
- * | 64 buckets * 4Byte |
- * #--------------------# <- 1280
- * |      alphabet      |
- * | 256 chars * 2Bytes | 512B
- * #--------------------# <- 1792
- * |    originalWord    |
- * | 64 * Uint16 = 128B |
- * #--------------------# <- 1920   - DATAOFFSET
- * |      licence       |           |
- * #--------------------#           |
- * |      alphabet      |           |
- * #--------------------#           |
- * |     STrieBits      |           |
- * #--------------------#           |
- * |     STrieChars     |           } pattern data (succinct value trie)
- * #--------------------#           |
- * |    hasValueBits    |           |
- * #--------------------#           |
- * |    valuesBitMap    |           |
- * #--------------------#           |
- * |       values       |           |
- * #--------------------# <- dataEnd-
- * |   alignment bytes  |
- * #--------------------# <- heapSize
- */
-
 "use strict";
 const {Transform} = require("assemblyscript/cli/transform");
 const fs = require("fs");
