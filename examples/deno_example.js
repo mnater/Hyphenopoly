f9a927fd5b053331d6ab4ccf1b25ff71f7deaa78
mnater <mathiasnater@gmail.com>
2021-12-29 23:06:43 +0100

    First steps in porting hyphenopoly to deno
    simple deno_example.js works, but tests are not yet running.
    
    [ci skip]

---
+++ examples/deno_example.js
@@ -0,0 +1,28 @@
+// For RunKit:
+// import hyphenopoly from "hyphenopoly";
+
+import hyphenopoly from "../hyphenopoly.deno.js";
+
+// For local node:
+// import hyphenopoly from "../hyphenopoly.module.js";
+
+const hyphenator = hyphenopoly.config({
+    "exceptions": {
+        "en-us": "en-han-ces"
+    },
+    "hyphen": "•",
+    "require": ["de", "en-us"]
+});
+
+async function hyphenateEn(text) {
+    const hyphenateText = await hyphenator.get("en-us");
+    console.log(hyphenateText(text));
+}
+
+async function hyphenateDe(text) {
+    const hyphenateText = await hyphenator.get("de");
+    console.log(hyphenateText(text));
+}
+
+hyphenateEn("hyphenation enhances justification.");
+hyphenateDe("Silbentrennung verbessert den Blocksatz.");
