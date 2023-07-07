3acf887d798845028d78d55d4a79110a5a2aa8b6
mnater <mathiasnater@gmail.com>
2023-07-07 22:45:21 +0200

    chore: emit sizes [ci skip]

--- tools/minify.sh
+++ tools/minify.sh
@@ -1,21 +1,23 @@
 #!/bin/sh
 # -*- coding: utf-8 -*-
 
-LOADEROSIZE=$(wc -c min/Hyphenopoly_Loader.js)
-H9YOSIZE=$(wc -c min/Hyphenopoly.js)
-
 echo 'old sizes:'
-echo $LOADEROSIZE
-echo $H9YOSIZE
+wc -c min/Hyphenopoly_Loader.js
+wc -c min/Hyphenopoly.js
+#wc -c min/Hyphenopoly_Loader.js.zip
+#wc -c min/Hyphenopoly.js.zip
 
 mkdir -p min
 
-terser Hyphenopoly_Loader.js -o min/Hyphenopoly_Loader.js --comments -c unsafe -m --warn
+terser Hyphenopoly_Loader.js -o min/Hyphenopoly_Loader.js --comments -c passes=3,unsafe -m --warn
 terser Hyphenopoly.js -o min/Hyphenopoly.js --comments -c passes=3,unsafe -m --warn
-
+#zip min/Hyphenopoly_Loader.js.zip min/Hyphenopoly_Loader.js
+#zip min/Hyphenopoly.js.zip min/Hyphenopoly.js
 echo 'new sizes:'
 wc -c min/Hyphenopoly_Loader.js
 wc -c min/Hyphenopoly.js
+#wc -c min/Hyphenopoly_Loader.js.zip
+#wc -c min/Hyphenopoly.js.zip
 
 cp -R patterns/ min/patterns
 cp -R testsuite/ min/testsuite
