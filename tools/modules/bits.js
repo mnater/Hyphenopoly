12a04be0f3503bd0fd5d05a376c86cea7e568fa2
mnater <mathiasnater@gmail.com>
2024-05-04 23:32:32 +0200

    chore: simplify asUint8ArraySwapped by calling asUint8Array
    [ci skip]

--- tools/modules/bits.js
+++ tools/modules/bits.js
@@ -57,13 +57,7 @@ export default function bits() {
     }
 
     function asUint8ArraySwapped() {
-        const bitString = data.join("");
-        let pos = 0;
-        const bytes = [];
-        while (pos < bitString.length) {
-            bytes.push(parseInt(bitString.slice(pos, pos + 8).padEnd(8, "0"), 2));
-            pos += 8;
-        }
+        const bytes = asUint8Array();
         const swapped = [];
         bytes.forEach((val, k) => {
             swapped[(k - (k % 8) + 7) - (k % 8)] = val;
